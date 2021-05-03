1042511be4cc1cec51b958eaba663fdadd547d8b
Ubuntu <ubuntu@ip-172-31-22-54.us-east-2.compute.internal>
2021-05-03 10:22:15 +0000

    changing package

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const express = require('express')
 const fs = require('fs');
 const app = express()
+var path = require('path');
 
 const port = 3001
 app.get('/product', (request, response) => {
-    let data = fs.readFileSync('./statics/product_des.json');
-    response.status(200).json(data)
+    fs.readFile('./statics/product_des.json', (err, data) => {
+        if (err) {
+            console.error(err)
+            return
+        }
+        let json_data = JSON.parse(data);
+        response.header("Access-Control-Allow-Origin", "*");
+        response.status(200).json(json_data)
+    })
 })
+
+var dir = path.join('./statics', 'product_img');
+
+
+app.use(express.static(dir));
 
 app.listen(port, (err) => {
     if (err) {
         return console.log('something bad happened', err)
     }
 
-}) 
+})
